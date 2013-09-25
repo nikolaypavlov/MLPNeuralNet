bd2942beb68188f0051f461f2ce643e35fcb967e
Nikolay Pavlov <me@nikolaypavlov.com>
2013-09-25 13:23:43 +0300

    Change my name to ukrainian

--- MLPNeuralNet.h
+++ MLPNeuralNet.h
@@ -2,7 +2,7 @@
 //  MLPNeuralNet.h
 //  BIOMotionTracker
 //
-//  Created by Nikolay Pavlov on 9/23/13.
+//  Created by Mykola Pavlov on 9/23/13.
 //  Copyright (c) 2013 Biomech, Inc. All rights reserved.
 //
 
